5cf388562ba3f2307e2169abd9d011ac12064bee
mercurysn <sdhf452erh>
2015-06-01 22:05:50 +1000

    https://github.com/mercurysn/SSACAdmin

---
+++ SSACAdmin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SSACAdmin.MvcApplication" Language="C#" %>
---
+++ SSACAdmin/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SSACAdmin.Startup))]
+namespace SSACAdmin
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
